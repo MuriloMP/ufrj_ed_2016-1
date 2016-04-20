699e6f36f9c9e5067e31c0bb1293209f072bef1b
leonardogoncalves <leonardo.goncalves.2711@gmail.com>
2016-04-19 23:12:24 -0300

    First Work functional

--- PrimeiroTrabalho/main2.c
+++ PrimeiroTrabalho/main2.c
@@ -20,30 +20,34 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct _node {
+typedef struct Word {
     char word[50];
     int count;
-    struct _node *next;
-    struct _node *previous;
-} ;
-
-typedef struct _node List;
+} Word;
 
 int main(int argc, char const *argv[]) {
 
-    List *head_node;
+    Word wordList[6000];
     char line[100];
+    int i = 0;
+    int j = 0;
+    int k = 0;
 
-    head_node = (List*)malloc(sizeof(List));
-
-    if (!head_node) {
-        printf("você alocou errado seu filho da puta\n");
+    while( (scanf("%[^\n]%*c", &line) == 1) ) {
+        strcpy(wordList[i].word, line);
+        wordList[i].count = 1;
+        i += 1;
     }
-    else {
-        head_node->next = NULL;
-        head_node->previous = NULL;
-        free(head_node);
+    
+    for( j = 0; j < i; j++ ){
+      for( k = 0; k < j; k++ ){
+        if( strcmp(wordList[j].word,wordList[k].word) > 1 ){
+            wordList[j].count += 1;
+        }
+      }
+      printf("%s %d ", wordList[j].word, wordList[j].count);
     }
+    
 
     return 0;
 }
