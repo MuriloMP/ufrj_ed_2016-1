0cb3c9a2e721379901ae251083d7535f8b01e2a9
leonardogoncalves <leonardo.goncalves.2711@gmail.com>
2016-04-19 23:12:24 -0300

    First Work functional

--- PrimeiroTrabalho/main.c
+++ PrimeiroTrabalho/main.c
@@ -1,12 +1,12 @@
 /*
  * Author: Leonardo Gonçalves
  * Data: 16/04/2016
- * Descript: O programa deverá receber uma lista de palavras 
- * de um arquivo de texto, avaliar quantas vezes cada 
+ * Descript: O programa deverá receber uma lista de palavras
+ * de um arquivo de texto, avaliar quantas vezes cada
  * palavra foi utilizada e exibir as cinco palavras mais
  * repetidas no texto.
  *
- * Plano: 
+ * Plano:
  * 1. Utilizar uma struct para armazenar a palavra e a
  * quantidade de vezes que ela foi utilizada;
  * 2. Armazenar as structs em um array estático.
@@ -20,34 +20,70 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct Word {
-	char word[50];
+typedef struct _Word {
 	int count;
+	char word[100];
 } Word;
+
+//typedef struct _Word Word;
 
 int main(int argc, char const *argv[]) {
 
-	Word wordList[6000];
-	char line[100];
-	int i = 0;
-	int j = 0;
-	int k = 0;
+	Word *wordList;
 
-	while( (scanf("%[^\n]%*c", &line) == 1) ) {
-	    strcpy(wordList[i].word, line);
-	    wordList[i].count = 1;
-	    i += 1;
+	//int count = 0;
+	int i;
+	char word[100];
+	int hasWord = 0;
+	int n = 10;
+	int countWords = 0; //quantas palavras tem na lista
+
+	wordList = NULL;
+	wordList = ( Word* )malloc( n * sizeof( Word ) );
+
+	if ( !wordList ) {
+		printf("Memória insuficiente\n");
 	}
-	
-	for( j = 0; j < i; j++ ){
-	  for( k = 0; k < j; k++ ){
-	    if( strcmp(wordList[j].word,wordList[k].word) > 1 ){
-			wordList[j].count += 1;
-	    }
-	  }
-	  printf("%s %d ", wordList[j].word, wordList[j].count);
+	else {
+		while(scanf( "%[^\n]%*c", word ) == 1) {
+			for (i = 0; i < countWords; i++) {
+				if ( (strcmp(wordList[i].word, word) == 0) ) {
+					wordList[i].count += 1;
+					hasWord = 1;
+				}
+			}
+
+			if (!hasWord) {
+				if (countWords == n) {
+					n += 10;
+					realloc(wordList, ((n + 10) * sizeof(Word)) );
+				}
+				strcpy(wordList[countWords].word, word);
+				wordList[countWords].count = 1;
+				countWords ++;
+			}
+			hasWord = 0;
+		}
 	}
+
+	int x, y;
+	Word temp;
 	
+	for ( x = 0 ; x < (countWords); x++ ) {
+		for ( y = x+1 ;  y < countWords; y++ ) {
+			if ( wordList[y].count > wordList[x].count ){
+				temp = wordList[y];
+				wordList[y] = wordList[x];
+				wordList[x] = temp;
+			}
+		}
+	}
+
+	for (i = 0; i  <= 14; i++) {
+		printf("%s %d \n", wordList[i].word, wordList[i].count );
+	}
+
+	free(wordList);
 
 	return 0;
 }
