b8e200d3e4a3ec479b09f630ccddfa98fc7b5c8a
Leonardo Gonçalves <leonardo.goncalves.2711@gmail.com>
2016-05-04 14:27:24 -0300

    fixing minor problems

--- SegundoTrabalho/main.c
+++ SegundoTrabalho/main.c
@@ -1,83 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX 128
+
 struct _node {
-    int key;
     int value;
     struct _node *next;
-    struct _node *prev;
 };
 
 typedef struct _node Node;
 
-Node *head = NULL;
-Node *last = NULL;
-Node *node = NULL;
-
-void insertFirst(int key, int value);
-void insertInLast(int key, int value);
-void printList();
-
 int main(int argc, char const *argv[]) {
-    
-    int key, value;
+    int key, value, i, count;
+    int occurrencyList[MAX];
+    Node *tmp = (Node*)malloc(sizeof(Node));
+    Node List[MAX];
 
-    while(scanf( "%d %d", &key, &value ) != EOF){
-    	if( key == 0 && value == 0 ) break;
-
-    	if( head == NULL ) {
-    	    insertFirst(key, value);
-    	} else {
-            insertInLast(key, value);
-        }
+    //initialize vector
+    for (i = 0; i < MAX; i++) {
+        List[i].next = NULL;
+        occurrencyList[i] = 0;
     }
 
-    // for (i = 0; i < 10; i++) {
-    //     insertInLast(i);
-    // }
+    while(scanf( "%d %d", &key, &value ) != EOF) {
+        if(key == 0 && value == 0) break;
 
-    printList();
+        occurrencyList[value]++;
+        count++;
+
+        Node *ptr = (Node*)malloc(sizeof(Node) );
+
+        ptr->value = value;
+        ptr->next = NULL;
+
+        if(List[key].next == NULL) {
+            List[key].next = ptr;
+        } else {
+            tmp = List[key].next;
+            while( tmp->next != NULL ) {
+                tmp = tmp->next;
+            }
+            tmp->next = ptr;
+        }     
+    }
+
+    for ( i = 1; i <= count; ++i) {
+        printf("%d: %d\n", i, occurrencyList[i]);
+    }
+
+    printf("Fim de programa\n");
     
     return 0;
-}
-
-void insertFirst(int key, int value){
-    Node *link = (Node*) malloc(sizeof(Node));
-    link->key = key;
-    link->value = value;
-
-    if (head == NULL) {
-        last = link;
-    } else {
-      //update first prev link
-      head->prev = link;
-   }
-
-   //point it to old first link
-   link->next = head;
-    
-   //point first to new first link
-   head = link;
-}
-
-void insertInLast(int key, int value) {
-    Node *link = (Node*)malloc(sizeof(Node));
-    link->key = key;
-    link->value = value;
-
-    if (head == NULL) {
-        last = link;
-    } else {
-        last->next = link;
-        link->prev = last;
-    }
-    last = link;
-}
-
-void printList() {
-    Node *ptr = head;    
-    while(ptr != NULL) {
-        printf("%d %d \n",ptr->key, ptr->value);
-        ptr = ptr->next;
-    }   
 }
