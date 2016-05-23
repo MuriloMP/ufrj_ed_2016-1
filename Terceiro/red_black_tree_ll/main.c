3a553d92e6d924f2fcc7c83442d97f3e72a969f1
Leonardo Gonçalves <leonardo.goncalves.2711@gmail.com>
2016-05-22 23:50:05 -0300

    i try

--- Terceiro/red_black_tree_ll/main.c
+++ Terceiro/red_black_tree_ll/main.c
@@ -1,162 +1,110 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-struct rbllNode{
+struct Node {
     int key;
-    char color;
-    struct rbllNode *esquerda;
-    struct rbllNode *direita;
-    struct rbllNode *parent;
+    struct Node *esq;
+    struct Node *dir;
 };
-typedef struct rbllNode rbllNode;
 
-rbllNode* root = NULL;
+typedef struct Node* Tree;
+typedef struct Node Node;
 
-void esquerdaRotaciona(struct rbllNode *root,struct rbllNode *x);
-void direitaRotaciona(struct rbllNode *root,struct rbllNode *y);
-void rbInsertFix(struct rbllNode *root,struct rbllNode *z);
-void rbInsert(int val);
-void inorderTree(struct rbllNode* root);
+Tree* create(Tree* root);
+void destroyNode(Node* node);
+void destroyTree(Tree* root);
+void showInPreOrder(Tree* root);
+void insert(Tree* root, int value);
 
-int main(int argc, char const *argv[]) {    
-    int loop = 1;
-    while(loop){
-        printf("\nRed Black Tree Management - Enter your choice : ");
-        printf("\n1\tInsert into arvorern\n2\tDisplay arvorern inorder\n");
-        int choice;
-        int val;
-        scanf("%d",&choice);
-        while(1){
-            scanf("%d",&val);
-            rbInsert(val);
-        }
-        printf("\nPress '0' to terminate and '1' to continue : ");
-        scanf("%d",&loop);
-    }
+int main(int argc, char const *argv[])
+{
+    Tree* root;    
+    rootcreate(root);   
+    
+    insert(root, 7);
+    showInPreOrder(root);
+
+    destroyTree(root);
     return 0;
 }
 
-
-void esquerdaRotaciona(struct rbllNode *root,struct rbllNode *x){
-    struct rbllNode *y;
-    y = x->direita;  
-    x->direita = y->esquerda; 
-    if( y->esquerda != NULL){
-        y->esquerda->parent = x; 
-    }
-    y->parent = x->parent;
-    if( x->parent == NULL){
-        root = y;
-    }
-    else if( x->key == x->parent->esquerda->key){
-        x->parent->esquerda = y; 
-    }
-    else x->parent->direita = y;
-    y->esquerda = x; 
-    x->parent = y; 
-
-    return;
+Tree* create(Tree* root){
+    root = (Tree*) malloc(sizeof(Tree));
+    if (root != NULL) {
+        *root = NULL;
+    }    
+    return root;
 }
 
-void direitaRotaciona(struct rbllNode *root,struct rbllNode *y){
-    struct rbllNode *x;
-    x = y->esquerda; 
-    y->esquerda = x->direita; 
-    if ( x->direita != NULL){
-        x->direita->parent = y;
-    }
-    x->parent = y->parent; 
-    if( y->parent == NULL){
-        root = x;
-    } 
-    else if( y->key == y->parent->esquerda->key){
-        y->parent->esquerda = x; 
-    }
-    else y->parent->direita = x;
-    x->direita = y; 
-    y->parent = x; 
-
-    return;
-
-}
-
-void rbInsertFix(struct rbllNode *root,struct rbllNode *z){
-    struct rbllNode *y;
-    while (z->parent->color == 'r'){
-        if (z->parent->key == z->parent->parent->esquerda->key){
-            y = z->parent->parent->direita;
-            if (y->color == 'r'){
-                z->parent->color = 'b';
-                y->color = 'b';
-                z->parent->parent->color = 'r';
-                z = z->parent->parent;
-            }
-            else if (z->key == z->parent->direita->key){
-                z = z->parent;
-                esquerdaRotaciona(root,z);
-            }
-            z->parent->color = 'b';
-            z->parent->parent->color = 'r';
-            direitaRotaciona(root,z->parent->parent);
-        }
-        else {
-            y = z->parent->parent->esquerda;
-            if (y->color == 'r'){
-                z->parent->color = 'b';
-                y->color = 'b';
-                z->parent->parent->color = 'r';
-                z = z->parent->parent;
-            }
-            else if (z->key == z->parent->esquerda->key){
-                z = z->parent;
-                direitaRotaciona(root,z);
-            }
-            z->parent->color = 'b';
-            z->parent->parent->color = 'r';
-            esquerdaRotaciona(root,z->parent->parent);
-        }
-    }
-    root->color = 'b';
-}
-
-void rbInsert(int val){
-    struct rbllNode *z = (struct rbllNode*)malloc(sizeof(struct rbllNode));
-    z->key = val;
-    z->esquerda = NULL;
-    z->direita = NULL;
-    z->color = 'r';
-    struct rbllNode *x = root;
-    struct rbllNode *y;
-    if ( root == NULL ){
-        root = z;
-        root->color = 'b';
+void destroyNode(Node* node){
+    if (node == NULL) {
         return;
     }
-    while ( x != NULL){
-        y = x;
-        if ( z->key < x->key){
-            x = x->esquerda;
-        }
-        else x = x->direita;
-    }
-    z->parent = y;
-    if ( y == NULL){
-        root = z;
-    }
-    else if( z->key < y->key ){
-        y->esquerda = z;
-    }
-    else y->direita = z;
-    rbInsertFix(root,z);
-
-    return;
+    destroyNode(node->esq);
+    destroyNode(node->dir);
+    free(node);
+    node = NULL;
 }
 
-void inorderTree(struct rbllNode* root){
-    struct rbllNode* temp = root;
-    if (temp != NULL){
-        inorderTree(temp->esquerda);
-        printf(" %d-%c ",temp->key,temp->color);
-        inorderTree(temp->direita);
+void destroyTree(Tree* root){
+    if (root == NULL) {
+        return;
     }
+    destroyNode(*root);
+    free(root);    
+}
+
+void showInPreOrder(Tree* root){
+    if (root == NULL) {    
+        return;
+    }
+    if (root != NULL) {
+        printf("%d ", (*root)->key);
+        showInPreOrder(&((*root)->esq));
+        showInPreOrder(&((*root)->dir));
+    }
+}
+
+void insert(Tree* root, int value){
+    /*verify if tree is valid*/
+    if (root == NULL) {
+        return;
+    }
+    
+    /*create a new node with new value */
+    Node* newNode = (Node*)malloc(sizeof(Node));
+
+    if (newNode == NULL) {
+        return;
+    }
+    newNode->key = value;
+    newNode->dir = NULL;
+    newNode->esq = NULL;
+
+    /*let's play*/
+
+    if (*root == NULL) {
+        *root = newNode;
+    }
+    else{
+        Node* currentNode = *root;
+        Node* previousNode = NULL;
+
+        while(currentNode != NULL){
+            previousNode = currentNode;
+            if (value == currentNode->key) {
+                free(newNode);
+                return;
+            }
+            if (value > newNode->key)
+                currentNode = currentNode->dir;            
+            else
+                currentNode = currentNode->esq;            
+        }
+        if (value > currentNode->key) 
+            previousNode->dir = newNode;        
+        else
+            previousNode->esq = newNode;
+    }
+    return;
 }
